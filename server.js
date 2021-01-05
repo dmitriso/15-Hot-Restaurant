96a39c4b57fa06f9c889e9985718411817c2b1e1
Dmitri So <dmitriso@yahoo.com>
2021-01-05 10:37:40 -0500

    Creating Routes for HTML and APIs

--- server.js
+++ server.js
@@ -10,13 +10,29 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // HTML ROUTES
+app.get('/tables', function(req,res) {
+    res.sendFile(path.join(__dirname, './tables.html'));
+});
 
+app.get('/reserve', function(req,res) {
+    res.sendFile(path.join(__dirname, './reserve.html'));
+});
+
+app.get('*', function(req,res) {
+    res.sendFile(path.join(__dirname, './index.html'));
+});
 
 
 
 
 // API ROUTES
+app.get('/api/waitlist', (req,res) => {
+    
+})
 
+app.get('/api/table', (req,res) => {
+
+})
 
 // SERVER LISTEN
 app.listen(PORT, function() {
