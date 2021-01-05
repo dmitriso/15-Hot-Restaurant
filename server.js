a9084b31b29daf40e4cceaab62429f6e6390ec53
Dmitri So <dmitriso@yahoo.com>
2021-01-05 10:55:59 -0500

    Commit before pull

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const path = require("path");
 
-
-
 var app = express();
 var PORT = process.env.PORT || 3000;
 
@@ -10,31 +8,62 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // HTML ROUTES
-app.get('/tables', function(req,res) {
+app.get('/tables', function (req, res) {
     res.sendFile(path.join(__dirname, './tables.html'));
 });
 
-app.get('/reserve', function(req,res) {
+app.get('/reserve', function (req, res) {
     res.sendFile(path.join(__dirname, './reserve.html'));
 });
 
-app.get('*', function(req,res) {
-    res.sendFile(path.join(__dirname, './index.html'));
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, './view.html'));
 });
 
 
 
 
+
+var tables = [
+    {
+        customerName: "Ahmed",
+        customerEmail: "ahmed@example.com",
+        customerID: "afhaque89",
+        phoneNumberww: "000-000-0000"
+    }
+]
+
+var waitlist = [
+    {
+    customerName: "Saima",
+    customerEmail: "saima@example.com",
+    phoneNumber: "000-000-0000",
+    customerID: "saimaCool"
+    }
+    ]
+
 // API ROUTES
-app.get('/api/waitlist', (req,res) => {
+app.get('/api/waitlist', (req, res) => {
+    return res.json(waitlist);
+});
+
+app.get('/api/table', (req, res) => {
+    return res.json(tables);
+});
+
+app.post('/api/table', (req,res) => {
+    var newTable = req.body;
     
+    console.log(newTable);
+
+    tables.push(newTable);
+
+    res.json(newTable);
+
 })
 
-app.get('/api/table', (req,res) => {
-
-})
 
 // SERVER LISTEN
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
-  });
+});
